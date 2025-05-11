dd238279f18aef12c82072e0cc73a7d52342438a
User <manyagolovko@yandex.ru>
2025-05-11 12:51:48 +0300

    Добавлен класс Note

---
+++ NoteApp/Note.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NoteApp
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public Note(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            Date = DateTime.Now;
+        }
+
+        public Note(string title, string content, DateTime date) : this(title, content)
+        {
+            Date = date; // Явно присваиваем Date из параметра
+        }
+    }
+}
